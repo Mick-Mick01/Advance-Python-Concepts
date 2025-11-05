40cd3f1ff435d00f6920d3738f5c7fb4143d2373
Mick-Mick01 <devkha8721@gmail.com>
2025-11-05 19:34:58 +0530

    Checking mutable objects in python hence all objects are passed by reference in a function in python.

--- mainPython.py
+++ mainPython.py
@@ -1,1 +1,22 @@
-print(type(None))
+# Checking if list and dictionaries are mutable or not. We have already seen that list are mutable in out projects
+
+myDict = {
+    "name":"Dev",
+    "age":45
+}
+myLis = ['d', 'd', 'y', 'e']
+
+
+# I WILL LEARN ALL THE FUNCTONS APPLICABLE TO A DICTIONARY LATER ON-!!
+def myFunc(myDict):
+    for key, value in myDict.items():
+        myDict[key] = "Harry"
+
+def myFunc2(lis):
+    for i in range(len(lis)):
+        lis[i] = 'a'
+
+myFunc(myDict)
+print(myDict)
+myFunc2(myLis)
+print(myLis)
