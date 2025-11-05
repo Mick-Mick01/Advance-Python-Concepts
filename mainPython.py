9e63d7dd8d24a78236e524cacc6bd63cd2f2e9e7
Mick-Mick01 <devkha8721@gmail.com>
2025-11-05 19:53:54 +0530

    What are Keyword & Positional arguments to a function. I till now only knew about positional arguments.

--- mainPython.py
+++ mainPython.py
@@ -1,39 +1,12 @@
 '''
-# *args and **kwargs
-# Name does not matter just the starts matters
+Let there be a function great, where age and name are two parameters
 
-#  We use *args to take variable number of arguments in a function
-#  *args is of type tuple
-#  Print(type(args))
+def greet(name, age):
+    print("Your name is: ",name," and your age is: ", age)
 
-#  **KWARGS IS OF TYPE DICTIONARY
+    Arguments as positional arguments
+    greet(age, name) ==> position of each argument matters, that's what I always use
+
+    Arguments as keyword arguments
+    greet(name="Alice", age=25)  # order doesn’t matter
 '''
-
-'''
-        *ARGS
-'''
-def specialFunction(*args):
-    print(type(args))
-    for values in args:
-        print(values)
-
-specialFunction("harry", 56, 9900, 99.007, "hello")
-print('\n')
-specialFunction(45, "Dev", "Mohan", 556.404, "humans")
-
-'''
-        **KWARGS
-'''
-
-def specialFunction2(**kwargs):
-    print(type(kwargs))
-    for key, value in kwargs.items():
-        print(key, value)
-
-Dict1 = { "name":"harry", "age":24 }
-Dict2 = { "name":"Dev", "age":20 }
-
-specialFunction2(**Dict1)
-print('\n')
-specialFunction2(**Dict2)
-
