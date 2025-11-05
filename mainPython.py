e22288966ab53fe3b97f349666cb695a873de545
Mick-Mick01 <devkha8721@gmail.com>
2025-11-05 20:02:38 +0530

    How to use *args, **kwargs together

--- mainPython.py
+++ mainPython.py
@@ -1,12 +1,17 @@
 '''
-Let there be a function great, where age and name are two parameters
-
-def greet(name, age):
-    print("Your name is: ",name," and your age is: ", age)
-
-    Arguments as positional arguments
-    greet(age, name) ==> position of each argument matters, that's what I always use
-
-    Arguments as keyword arguments
-    greet(name="Alice", age=25)  # order doesn’t matter
+HOW TO PASS ALL THREE TYPES OF PARAMETERS IN A FUNCTION IN PYTHON
+Always write normal parameters first(as keyword/positional arguments), then write *args & then write **kwargs. This order should be followed
 '''
+
+name = "Harry"
+
+def myFunc(name, *args, **kwargs):
+    print(name)
+    
+    for parameter in args:
+        print(parameter)
+    
+    for key, value in kwargs.items():
+        print(key,':', value)
+
+myFunc("harry", *["hi", "oh"], **{"user_name":"dev", "age":20})
